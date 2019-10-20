7f332af9eb6ec7c3ff9d4bb72cc3c58287bf098e
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2019-10-20 09:06:58 -0700

    Add files via upload

---
+++ precache-manifest.73801116119f9ba57a411506f3d034c5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95622dfb2d81206d4f10f5ffadf570c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cb87b82d8a897c1d1f",
+    "url": "/static/css/main.6d28b8bc.chunk.css"
+  },
+  {
+    "revision": "5852a3285ae307911fc1",
+    "url": "/static/js/2.078a0b67.chunk.js"
+  },
+  {
+    "revision": "66cb87b82d8a897c1d1f",
+    "url": "/static/js/main.f4ad3680.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6e7521089fa217df88b5bbf6e1e763b",
+    "url": "/static/media/bebop.f6e75210.png"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "faf2cf2d266376349d8e17c98d8f174d",
+    "url": "/static/media/camera1.faf2cf2d.png"
+  },
+  {
+    "revision": "cb5a07c7d872221c9e79d8a583d3daf3",
+    "url": "/static/media/controller.cb5a07c7.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "149753a2cf24d8ffa7697e86cffc94db",
+    "url": "/static/media/lipo1.149753a2.png"
+  },
+  {
+    "revision": "7792c21b3e3a53723f643e7d75d21d3b",
+    "url": "/static/media/lipo2.7792c21b.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "edcfffca967cdd0e32bc270628fd8637",
+    "url": "/static/media/parot.edcfffca.png"
+  }
+]);
